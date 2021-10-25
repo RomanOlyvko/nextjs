47141932ae5e68d056aeae643889990c05f287af
Roman Olyvko <roman@gmail.com>
2021-10-25 23:37:36 +0300

    Hello step-1

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,6 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
